9ad1e19557e02a37c99bba3b47184ab3104de1da
ifonz <ifonz@DESKTOP-DAIMD7H>
2021-12-07 10:17:01 +0100

    day06

--- advent-of-code-2021/src/main/java/it/ifonz/puzzles/Day06.java
+++ advent-of-code-2021/src/main/java/it/ifonz/puzzles/Day06.java
@@ -3,8 +3,9 @@ package it.ifonz.puzzles;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import it.ifonz.common.AbstractDay;
@@ -42,22 +43,36 @@ public class Day06 extends AbstractDay {
 		System.out.println(fishes.size());
 	}
 
+	private Map<Integer,Long> cache = new HashMap<Integer,Long>();
+	
 	public void part2() {
 		var fishes = new ArrayList<>(numbers);
-		for (var i = 0; i < 80; i++) {
-			System.out.println(fishes);
-			List<Integer> newFishes = new ArrayList<>();
-			for (var j = 0; j < fishes.size(); j++) {
-				var fish = fishes.get(j)-1;
-				if (fish < 0) {
-					newFishes.add(8);
-					fish = 6;
-				}
-				fishes.set(j, fish);
+		long sum = fishes.size();
+		for (var fish : fishes) {
+			var cachedKids = cache.get(fish);
+			if (cachedKids != null) {
+				sum += cachedKids;
+			} else {
+				var kids = howManyKids(256, fish);
+				sum += kids;
+				cache.put(fish,kids);
 			}
-			fishes.addAll(newFishes);
 		}
-		System.out.println(fishes.size());
+
+		System.out.println(sum);
+	}
+	
+	
+	private long howManyKids(int daysLeft, int start) {
+		if (daysLeft < 9) return 0;
+		
+		daysLeft-=(start+1);
+		var s = 1+howManyKids(daysLeft,8);
+		while(daysLeft>=7) {
+			daysLeft-=7;
+			s+=howManyKids(daysLeft,8)+1;
+		}
+		return s;
 	}
 
 }
