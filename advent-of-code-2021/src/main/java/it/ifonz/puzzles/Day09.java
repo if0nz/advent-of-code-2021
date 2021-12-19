e72e69d9c08b4dbe97ec1323eebcb4582954621c
ifonz <gdifonzo89@gmail.com>
2021-12-19 15:30:58 +0100

    day09

--- advent-of-code-2021/src/main/java/it/ifonz/puzzles/Day09.java
+++ advent-of-code-2021/src/main/java/it/ifonz/puzzles/Day09.java
@@ -1,10 +1,10 @@
 package it.ifonz.puzzles;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import it.ifonz.common.AbstractDay;
 import it.ifonz.common.FileReader;
@@ -39,6 +39,7 @@ public class Day09 extends AbstractDay {
 		System.out.println(lowpoints.chars().mapToLong(Character::getNumericValue).sum() + lowpoints.length());
 	}
 
+	// given a digit, checked whether it's less than a neighbor value
 	private boolean check(int digit, int x, int y) {
 		try {
 			return digit < flows.get(x).charAt(y);
@@ -46,36 +47,55 @@ public class Day09 extends AbstractDay {
 			return true;
 		}
 	}
+	
+	// given a location, checkes wheter it's 9 or it's out of the box and, if so, it's not been already visited
+	private boolean check(int x, int y, HashSet<Coord> visited) {
+		try {
+			return flows.get(x).charAt(y) != '9' && !visited.contains(new Coord(x,y));
+		} catch (Exception e) {
+			return false;
+		}
+	}
 
 	public void part2() {
 		var basins = new ArrayList<Integer>();
 		var len = flows.get(0).length();
+//		for (var i = 0; i < flows.size(); i++) { // vertical index
+//			for (var j = 0; j < len; j++) { // horizontal index
+//				var current = flows.get(i).charAt(j);
+//				if (check(current, i, j + 1) && check(current, i, j - 1) && check(current, i - 1, j)
+//						&& check(current, i + 1, j)) {
+//					int basinSize = 1;
+//					var z = 0;
+//					// find left
+//					while (j - (++z) > 0 && flows.get(i).charAt(j-z) < '9') {
+//						basinSize++;
+//					}
+//					z = 0;
+//					// find right
+//					while (j + (++z) < len && flows.get(i).charAt(j+z) < '9') {
+//						basinSize++;
+//					}
+//					// find up
+//					z = 0;
+//					while (i-(++z) > 0 && flows.get(i-z).charAt(j) < '9') {
+//						basinSize++;
+//					}
+//					// find down
+//					while (i+(++z) < flows.size() && flows.get(i+z).charAt(j) < '9') {
+//						basinSize++;
+//					}
+//					basins.add(basinSize);
+//				}
+//			}
+//		}
+		
 		for (var i = 0; i < flows.size(); i++) { // vertical index
 			for (var j = 0; j < len; j++) { // horizontal index
 				var current = flows.get(i).charAt(j);
 				if (check(current, i, j + 1) && check(current, i, j - 1) && check(current, i - 1, j)
 						&& check(current, i + 1, j)) {
-					int basinSize = 1;
-					var z = 0;
-					// find left
-					while (j - (++z) > 0 && flows.get(i).charAt(j-z) < '9') {
-						basinSize++;
-					}
-					z = 0;
-					// find right
-					while (j + (++z) < len && flows.get(i).charAt(j+z) < '9') {
-						basinSize++;
-					}
-					// find up
-					z = 0;
-					while (i-(++z) > 0 && flows.get(i-z).charAt(j) < '9') {
-						basinSize++;
-					}
-					// find down
-					while (i+(++z) < flows.size() && flows.get(i+z).charAt(j) < '9') {
-						basinSize++;
-					}
-					basins.add(basinSize);
+					basins.add(visitTheBasin(i, j));
 				}
 			}
 		}
@@ -87,8 +107,28 @@ public class Day09 extends AbstractDay {
 		System.out.println(m1*m2*m3);
 	}
 	
-	private void visitTheBasin() {
-		
+	private int visitTheBasin(int i, int j) {
+		var queue = new ArrayDeque<Coord>();
+		var visited = new HashSet<Coord>();
+		queue.add(new Coord(i, j));
+		while (!queue.isEmpty()) {
+			var c = queue.poll();
+			visited.add(c);
+			if(check(c.x, c.y + 1, visited)) 
+				queue.add(new Coord(c.x,c.y+1));
+			if(check(c.x, c.y - 1, visited)) {
+				queue.add(new Coord(c.x, c.y - 1));
+			}
+			if(check(c.x - 1, c.y, visited)) {
+				queue.add(new Coord(c.x - 1, c.y));
+			}
+			if(check(c.x + 1, c.y, visited)) {
+				queue.add(new Coord(c.x + 1, c.y));
+			}	
+		}
+		return visited.size();
 	}
 
+	record Coord(int x, int y) {
+	}
 }
